cd601425dff5687194e330adad8c577260ad268f
Leandro Miranda Fahur Machado <leandrofahur@gmail.com>
2025-06-12 19:53:30 -0700

    updating the frontend

--- frontend/src/components/Visualizer/Visualizer.tsx
+++ frontend/src/components/Visualizer/Visualizer.tsx
@@ -6,7 +6,7 @@ export const Visualizer = ({ output }: VisualizerProps) => {
   return (
     <div className="w-1/2">
       <h1 className="text-3xl font-semibold mb-4 text-center">Visualizer</h1>
-      <div className="p-4 bg-gray-100 rounded-md h-[70vh] overflow-auto">
+      <div className="p-4 bg-gray-100 border-gray-300 border border-l-0 h-[70vh] overflow-auto">
         {output ? (
           <pre className="whitespace-pre-wrap break-words font-mono">
             {output}
